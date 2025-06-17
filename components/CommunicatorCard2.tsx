3d75f9c8d75f7b097481e3561ed7de5e3de0b51e
Pedro P. DevLawyer <486888@gmail.com>
2025-06-17 09:04:58 -0300

    Update CommunicatorCard2.tsx

--- components/CommunicatorCard2.tsx
+++ components/CommunicatorCard2.tsx
@@ -19,7 +19,7 @@ export default function CommunicatorCard2() {
       alt="Abrir App do Miau"
       className="w-16 h-16 rounded-md"
     />
-    <span className="text-xl font-semibold">Meus 10 meses de corrida</span>
+    <span className="text-xl font-semibold">interaja com 10 meses de dados de GPS corrida!</span>
   </a>
   
   <a
